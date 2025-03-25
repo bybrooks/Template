6fca738f8361c95c57dfa78783f90db699d03518
bybrooks <garimi0902@gmail.com>
2025-03-26 00:31:27 +0900

    feat: clang-formatの設定ファイルを追加

--- cpp/sort.cpp
+++ cpp/sort.cpp
@@ -5,3 +5,13 @@ void sort_ascending(vector<int> &nums1)
 {
     sort(nums1.begin(), nums1.end());
 }
+
+int removeElement(vector<int>& nums, int val) {
+    nums.erase(std::remove(nums.begin(), nums.end(), val), nums.end());
+    return nums.size();
+}
+
+int removeDuplicates(vector<int>& nums) {
+    nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
+    return nums.size();
+}
